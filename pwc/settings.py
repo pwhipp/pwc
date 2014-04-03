c8d4a3d7ce412776d9c24d6bfb632d6f11cbbe62
pwhipp <paul.whipp@gmail.com>
2014-04-03 16:10:28 +1000

    Updated media_root appropriately

--- pwc/settings.py
+++ pwc/settings.py
@@ -229,7 +229,7 @@ MEDIA_URL = STATIC_URL + "media/"
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, *MEDIA_URL.strip("/").split("/"))
+MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 
 # Package/module name to import the root urlpatterns from for the project.
 ROOT_URLCONF = "%s.urls" % PROJECT_DIRNAME

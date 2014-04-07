6b6f22f4f19f75fbb5a039be5b0929d2863d4555
pwhipp <paul.whipp@gmail.com>
2014-04-08 06:21:05 +1000

    Added paulwhippconsulting domains to ALLOWED.HOSTS

--- pwc/settings.py
+++ pwc/settings.py
@@ -107,8 +107,10 @@ MANAGERS = ADMINS
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['dev:8000',
                  '',
-                 'paulwhippconsulting.com',
-                 'www.paulwhippconsulting.com']
+                 '.paulwhippconsulting.com',
+                 '.paulwhippconsulting.com.',
+                 '.paulwhippconsulting.com.au',
+                 '.paulwhippconsulting.com.au.']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name

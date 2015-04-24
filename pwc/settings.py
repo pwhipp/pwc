3e19cbdca52e9fae2423c73e57ad72902c7e18f0
pwhipp <paul.whipp@gmail.com>
2015-04-25 06:43:48 +1000

    Added pwc/static to staticfiles dirs

--- pwc/settings.py
+++ pwc/settings.py
@@ -219,12 +219,8 @@ CACHE_MIDDLEWARE_KEY_PREFIX = PROJECT_DIRNAME
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = "/static/"
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_collected')
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
